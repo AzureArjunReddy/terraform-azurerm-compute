adda5dab087d4044b40871cec86533349ecc3046
Mallikarjuna Reddy Tunga <mallikarjunareddy.tunga@gmail.com>
2021-08-10 14:15:16 +0300

    Update WinVM.tf

--- win-vm/WinVM.tf
+++ win-vm/WinVM.tf
@@ -2,147 +2,102 @@
 # Developed by Tunga Mallikarjuna Reddy
 ###############################################################################
 
-locals {
-  server_name = "${var.vm_name}${var.app}"
-  dns_name    = lower("${var.app}")
+# Generate random text for a unique Public IP name
+resource "random_id" "randomId" {
+    byte_length = 5
 }
-
+# Read and Load Vnet Info
 data "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   resource_group_name = var.nw_rg_name
 }
+# Read and Load Subnet Info
 data "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   virtual_network_name = data.azurerm_virtual_network.vnet.name
   resource_group_name  = var.nw_rg_name
 }
+# Read and Load Keyvault Info
 data "azurerm_key_vault" "kv" {
   count               = var.disk_encryption_required ? 1 :0
   name                = var.keyvault_name
   resource_group_name = var.keyvault_rg_name
 }
+# Read and Load Keyvault KeK Info
 data "azurerm_key_vault_key" "key" {
   count               = var.disk_encryption_required ? 1 :0
   name                = var.key_name
   key_vault_id        = data.azurerm_key_vault.kv[0].id
 }
+# Create public IPs
+resource "azurerm_public_ip" "public_ip" {
+    name                         = "Arjun${random_id.randomId.hex}"
+    location                     = var.location_name
+    resource_group_name          = var.vm_rg_name
+    allocation_method            = "Dynamic"
 
-resource "azurerm_public_ip" "pip" {
-  count                   = var.public_ip_required == "true" ? var.node_count : 0
-  name                    = "${local.server_name}0${count.index + 1}-pip"
-  location                = var.location_name
-  resource_group_name     = var.vm_rg_name
-  allocation_method       = "Static"
-  sku                     = "Standard"
-  idle_timeout_in_minutes = 30
-  domain_name_label       = "${local.dns_name}0${count.index + 1}"
-  zones                   = [var.zones[count.index % length(var.zones)]]
-
-  tags = merge(var.tags, {"SubNet" : var.subnet_name})
-
+    tags = {
+        environment = "MSFT Reactor"
+    }
 }
+# Create network interface
 resource "azurerm_network_interface" "private_nic" {
-  count               = var.node_count
-  name                = "${local.server_name}0${count.index + 1}-nic01"  
+  name                = "${var.vm_name}-nic01"  
   location            = var.location_name
   resource_group_name = var.vm_rg_name
 
   ip_configuration {
-    name                          = "${local.server_name}0${count.index + 1}-ip1"
+    name                          = "${var.vm_name}-ip1"
     subnet_id                     = data.azurerm_subnet.subnet.id
-    private_ip_address_allocation = var.ip_type
-    private_ip_address            = var.ip_addr
-    public_ip_address_id          = var.public_ip_required == "true" ? azurerm_public_ip.pip[count.index].id : ""
+    private_ip_address_allocation = "Dynamic"    
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
-  enable_accelerated_networking = var.enable_accelerated_nw
-    
-  tags = merge(var.tags, {"SubNet" : var.subnet_name})
+  tags = {
+        environment = "MSFT Reactor"
+  }
 }
 
-resource "azurerm_virtual_machine" "vm" {
-  count                 = var.node_count
-  name                  = "${local.server_name}0${count.index + 1}"
-  location              = var.location_name
-  zones                 = [var.zones[count.index % length(var.zones)]]
+# Create Virtual Machine with RedHat MarketPlace Image
+resource "azurerm_virtual_machine" "vm" {  
+  name                  = var.vm_name
+  location              = var.location_name  
   resource_group_name   = var.vm_rg_name
-  network_interface_ids = [element(azurerm_network_interface.private_nic.*.id, count.index)]
+  network_interface_ids = [azurerm_network_interface.private_nic.id]
   vm_size               = var.vm_sku_type
-  license_type          = var.win_license_type != "" ? var.win_license_type : null
-  
-  delete_os_disk_on_termination    = var.delete_os_disk
-  delete_data_disks_on_termination = var.delete_data_disks
 
   storage_image_reference {
-    id        = var.os_cs_img_required == "true" ? var.os_cs_img_id : ""
-    publisher = var.os_cs_img_required == "true" ? "" : var.os_mk_img_publisher
-    offer     = var.os_cs_img_required == "true" ? "" : var.os_mk_img_offer
-    sku       = var.os_cs_img_required == "true" ? "" : var.os_mk_img_sku
-    version   = var.os_cs_img_required == "true" ? "" : var.os_mk_img_version
-  }
-  boot_diagnostics {
-    enabled     = "true"
-    storage_uri = var.diag_storage_name
+      publisher = "MicrosoftWindowsServer"
+      offer     = "WindowsServer"
+      sku       = "2019-Datacenter"
+      version   = "latest"
   }
   storage_os_disk {
-    name              = "${local.server_name}0${count.index + 1}-osdisk"
+    name              = "${var.vm_name}-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = var.osdisk_type
-    #disk_size_gb      = var.osdisk_size
+    managed_disk_type = var.osdisk_type    
   }
-  dynamic "storage_data_disk" {
-    for_each = var.data_disk_required ? [""] : []
+  dynamic storage_data_disk {
+    for_each = range(var.nbof_data_disk)
     content {
-      name              = "${local.server_name}0${count.index + 1}-ddisk01"
-      caching           = "ReadWrite"
+      name              = "${var.vm_name}-ddisk0${storage_data_disk.value +1}"      
       create_option     = "Empty"
-      lun               = 01
-      managed_disk_type = var.mddisk_type != "" ? var.mddisk_type : null
-      disk_size_gb      = var.mddisk_size != "" ? var.mddisk_size : null
+      lun               = storage_data_disk.value
+      disk_size_gb      = var.mddisk_size
+      managed_disk_type = var.mddisk_type      
     }
-  }
+  }  
   os_profile {
-    computer_name  = "${local.server_name}0${count.index + 1}"
-    admin_username = var.vm_username
-    admin_password = var.vm_password
+    computer_name  = var.vm_name
+    admin_username = "winadmin"
+    admin_password = "Passw0rd!123"
   }
   os_profile_windows_config {
     provision_vm_agent = true
-    timezone           = "Arab Standard Time"
+  }
+  boot_diagnostics {
+      enabled     = "true"
+      storage_uri = var.diag_storage_name
   }
   tags = var.tags
-}
-
-resource "azurerm_virtual_machine_extension" "nw-watcher" {
-  count                        = var.nw_watcher_required == "true" ? var.node_count : 0
-  name                         = "NetworkWatcher"
-  virtual_machine_id           = element(azurerm_virtual_machine.vm.*.id, count.index + 1)
-  publisher                    = "Microsoft.Azure.NetworkWatcher"
-  type                         = var.nwwType
-  type_handler_version         = "1.4"
-  auto_upgrade_minor_version   = true
-
-  depends_on = [azurerm_virtual_machine.vm]
-}
-
-resource "azurerm_virtual_machine_extension" "log_analytics" {
-  count                         = var.enable_log_analytics == "true" ? var.node_count : 0
-  name                          = "LogAnalytics"
-  virtual_machine_id            = element(azurerm_virtual_machine.vm.*.id, count.index + 1)
-  publisher                     = "Microsoft.EnterpriseCloud.Monitoring"
-  type                          = var.agntType
-  type_handler_version          = "1.0"
-
-   settings = <<SETTINGS
-     {
-         "workspaceId": ""
-     }
- SETTINGS
-   protected_settings = <<PROTECTED_SETTINGS
-     {
-         "workspaceKey": ""
-     }
- PROTECTED_SETTINGS
- 
-  depends_on = [azurerm_virtual_machine_extension.nw-watcher]
 }
